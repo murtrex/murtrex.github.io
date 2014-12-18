34eaea777ac43fc8e74b2b064eb865a7dfe7dbb3
murtrex <murtrex@gmail.com>
2014-12-18 03:48:24 -0500

    added angular and siimple blog cms

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+var murtrexBlog = angular.module('murtrexBlog', []);
+
+murtrexBlog.controller('feedController', ['$scope', function($scope){
+	$scope.blogs = [
+		{ url: "blogs/one.html" },
+	]
+}]);
