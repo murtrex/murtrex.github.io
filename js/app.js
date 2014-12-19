754129f5ba7ae7b73da32698c88cc5880ce020ee
murtrex <murtrex@gmail.com>
2014-12-19 17:22:51 -0500

    added routing functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 $(document).foundation();
 
-var murtrexBlog = angular.module('murtrexBlog', ['hc.marked']);
+var murtrexBlog = angular.module('murtrexBlog', ['hc.marked', 'ngRoute'])
 
-murtrexBlog.controller('feedController', ['$scope', function($scope){
+.controller('feedController', function($scope){
 	$scope.blogs = [
 		{ url: "blogs/one.md" },
 		{ url: "blogs/two.md" },
 	]
-}]);
+})
+
+.controller('blogController', function($scope, $routeParams){
+	$scope.params = $routeParams;
+})
+
+.config(function($routeProvider, $locationProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'feed.html',
+			controller: 'feedController'
+		})
+		.when('/blog/:blogID', {
+			templateUrl: 'blog.html',
+			controller: 'blogController'
+		});
+});
